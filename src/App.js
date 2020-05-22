7dcca4290c5036db4276317168807b7499be9777
Mukesh Yadav <mukeshyadav@gmail.com>
2020-05-22 18:52:52 +0530

    updating title using useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const App = () => {
   const [count, setCount] = useState(0);
@@ -7,6 +7,10 @@ const App = () => {
   const incrementCount = () => {
     setCount(prevCount => prevCount + 1);
   };
+
+  useEffect(() => {
+    document.title = `I have clicked ${count} times.`;
+  });
 
   const toggleLight = () => setIsOn(prevIsOn => !prevIsOn);
 
