ad100d9c888210165d27cc0c24c2efadc4a66e94
Mukesh Yadav <mukeshyadav@gmail.com>
2020-05-22 18:45:07 +0530

    adding toggle using useState hooks

--- src/App.js
+++ src/App.js
@@ -2,14 +2,31 @@ import React, { useState } from "react";
 
 const App = () => {
   const [count, setCount] = useState(0);
+  const [isOn, setIsOn] = useState(false);
 
   const incrementCount = () => {
     setCount(prevCount => prevCount + 1);
   };
+
+  const toggleLight = () => setIsOn(prevIsOn => !prevIsOn);
+
   return (
-    <>
+    <div className="wrapper">
+      <h2>Counter</h2>
       <button onClick={incrementCount}>I clicked {count}</button>
-    </>
+
+      <h2>Toggle Light</h2>
+      <img
+        onClick={toggleLight}
+        alt="Toggle Light"
+        src={
+          isOn
+            ? "https://icon.now.sh/highlight/fd0"
+            : "https://icon.now.sh/highlight/000"
+        }
+        style={{ height: "50px", width: "50px", cursor: "pointer" }}
+      />
+    </div>
   );
 };
 
